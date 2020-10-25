6ebe22333f728a9a7a0531269c3b646cbd36cf65
Daniel Santa <daniel.santar@udea.edu.co>
2020-10-25 08:36:53 -0500

    fix network

--- network.sh
+++ network.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo docker network create --driver overlay vendecrispetasproxy
-sudo docker network create --driver overlay taller
+sudo docker network create --driver overlay vendecrispetas
