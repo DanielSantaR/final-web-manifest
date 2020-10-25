f99b5d5e13b7b19fcb2c9038f84ded1c245191a9
Daniel Santa <daniel.santar@udea.edu.co>
2020-10-25 11:28:45 -0500

    manifest without portainer and lets encrypt

--- pull.sh
+++ pull.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-manifets=("jwilder/nginx-proxy" "jrcs/letsencrypt-nginx-proxy-companion" "portainer/portainer")
+# manifets=("jwilder/nginx-proxy" "jrcs/letsencrypt-nginx-proxy-companion" "portainer/portainer")
+manifets=("jwilder/nginx-proxy")
 for stack in "${manifets[@]}"
 do
  sudo docker pull $stack
--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ sudo bash ${PWD}/network.sh
 sudo bash ${PWD}/volume.sh
 
 sudo docker stack deploy -c ingress.stack.yaml taller_ingress
-sudo docker stack deploy -c portainer.stack.yaml taller_portainer
+# sudo docker stack deploy -c portainer.stack.yaml taller_portainer
